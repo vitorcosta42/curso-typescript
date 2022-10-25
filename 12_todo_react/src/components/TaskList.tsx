4f6a77a8eb096c2db14990e8935ffc78a0978360
Vitor <vitorcostadev@gmail.com>
2022-10-25 15:57:54 -0300

    novas aulas

--- 12_todo_react/src/components/TaskList.tsx
+++ 12_todo_react/src/components/TaskList.tsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React from "react";
 
-type Props = {}
+// Interfaces
+import { ITask } from "../interfaces/Task";
 
-const TaskList = (props: Props) => {
-  return (
-    <div>Lista de tarefas</div>
-  )
+// CSS
+import styles from "./TaskList.module.css";
+
+interface Props {
+  taskList: ITask[];
+  handleDelete(id: number): void;
+  handleEdit(task: ITask): void;
 }
 
-export default TaskList
+const TaskList = ({ taskList, handleEdit, handleDelete }: Props) => {
+  return (
+    <>
+      {taskList.length > 0 ? (
+        taskList.map((task) => (
+          <div key={task.id} className={styles.task}>
+            <div className={styles.details}>
+              <h4>{task.title}</h4>
+              <p>Dificuldade: {task.difficulty}</p>
+            </div>
+            <div className={styles.actions}>
+              <i
+                className="bi bi-pencil"
+                onClick={() => {
+                  handleEdit(task);
+                }}
+              ></i>
+              <i
+                className="bi bi-trash"
+                onClick={() => {
+                  handleDelete(task.id);
+                }}
+              ></i>
+            </div>
+          </div>
+        ))
+      ) : (
+        <p>Não há tarefas cadastradas!</p>
+      )}
+    </>
+  );
+};
+
+export default TaskList;
