42bdc43e32f2833a3a1ca38645dc3572be0b13ad
Vitor <vitorcostadev@gmail.com>
2022-11-09 15:57:50 -0300

    mais aulas

--- 13_express_com_ts/src/app.ts
+++ 13_express_com_ts/src/app.ts
@@ -2,12 +2,70 @@
 //console.log("Express + TS");
 
 //2 - init express
-import express from "express";
+import express, { Request, Response } from "express";
 
 const app = express();
 
+// 3 - rota com POST
+app.use(express.json());
+
 app.get("/", (req, res) => {
   return res.send("Hello Express!");
+});
+
+// 3 - rota com POST
+app.post("/api/product", (req, res) => {
+  console.log(req.body);
+  return res.send("Produto adicionado!");
+});
+
+// 4 - rota para todos os verbos
+app.all("/api/product/check", (req, res) => {
+  // req.method = VERBO HTTP
+  if (req.method === "POST") {
+    return res.send("Inseriu algum registro");
+  } else if (req.method === "GET") {
+    return res.send("Leu algum registro!");
+  } else {
+    return res.send("Não podemos realizar esta operação");
+  }
+});
+
+// 5 - interfaces do express
+app.get("/api/interfaces", (req: Request, res: Response) => {
+  return res.send("Utilizando as interfaces");
+});
+
+// 6 - enviando json
+app.get("/api/json", (req: Request, res: Response) => {
+  return res.json({
+    name: "Shirt",
+    price: 30.0,
+    color: "Blue",
+  });
+});
+
+// 7 - router parameters
+app.get("/api/product/:id", (req: Request, res: Response) => {
+  console.log(req.params);
+  const id = req.params.id;
+  if (id === "1") {
+    const product = {
+      id: 1,
+      name: "Boné",
+      price: 10,
+    };
+    return res.json(product);
+  } else if  (id === "2") {
+    const product = {
+      id: 2,
+      name: "Chapéu",
+      price: 20,
+    };
+    return res.json(product);
+  } else{
+    return res.send("Produto não encontrado!");
+  }
 });
 
 app.listen(3000, () => {
